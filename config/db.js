694a3197c04966f7f8d01de2411bf4c148dcb920
Moustafa <bendib.moustafa@gmail.com>
2019-11-12 23:52:03 +0100

    adding dtatabase configuration and connecting it using mongoose to MongoDB

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const monggose = require('mongoose');
+
+
+const connectDB = async () => {
+    const conn = await monggose.connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    });
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+}
+
+module.exports = connectDB;
