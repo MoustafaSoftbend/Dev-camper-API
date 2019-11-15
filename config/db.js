05afe11683993b2996e7cb75ceeaf5093d52cbeb
Moustafa <bendib.moustafa@gmail.com>
2019-11-15 10:35:51 +0100

    Add create Bootcamp from controllers

--- config/db.js
+++ config/db.js
@@ -1,12 +1,30 @@
-const monggose = require('mongoose');
+const mongoose = require('mongoose');
 
 
 const connectDB = async () => {
-    const conn = await monggose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useCreateIndex: true,
         useFindAndModify: false,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
+        server: {    
+            auto_reconnect: true,    
+            socketOptions: {     
+              keepAlive: 1,    
+              connectTimeoutMS: 90000,    
+              socketTimeoutMS: 90000,
+              maxTimeMS: 20000,    
+            }    
+            },    
+        replset: {    
+            auto_reconnect: true,    
+            socketOptions: {    
+                keepAlive: 1,    
+                connectTimeoutMS: 90000,    
+                socketTimeoutMS: 90000,
+                maxTimeMS: 20000,    
+            }    
+        }
     });
 
     console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
