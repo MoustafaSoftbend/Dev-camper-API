41c395023e9028303bceeaee6a73faeecfdbb71b
Moustafa <bendib.moustafa@gmail.com>
2019-11-19 17:01:37 +0100

    Add update & delete controllers

--- config/db.js
+++ config/db.js
@@ -7,24 +7,6 @@ const connectDB = async () => {
         useCreateIndex: true,
         useFindAndModify: false,
         useUnifiedTopology: true,
-        server: {    
-            auto_reconnect: true,    
-            socketOptions: {     
-              keepAlive: 1,    
-              connectTimeoutMS: 90000,    
-              socketTimeoutMS: 90000,
-              maxTimeMS: 20000,    
-            }    
-            },    
-        replset: {    
-            auto_reconnect: true,    
-            socketOptions: {    
-                keepAlive: 1,    
-                connectTimeoutMS: 90000,    
-                socketTimeoutMS: 90000,
-                maxTimeMS: 20000,    
-            }    
-        }
     });
 
     console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold);
