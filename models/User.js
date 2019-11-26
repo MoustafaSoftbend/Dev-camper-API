eb79ff86903538a97ca9996c0521818a1842a10c
Moustafa <bendib.moustafa@gmail.com>
2019-11-26 18:40:33 +0100

    Add login controller and setting the jwtToken to be stored on a cookie

--- models/User.js
+++ models/User.js
@@ -45,6 +45,11 @@ UserSchema.methods.getSignedJwtToken = function () {
     return jwt.sign({id: this._id}, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRE
     });
+};
+
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function (enteredPAssword) {
+    return await bcrypt.compare(enteredPAssword, this.password);
 }
 
 module.exports = mongoose.model('User', UserSchema);
