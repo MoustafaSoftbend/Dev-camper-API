fe72142df63d30e2dcf7293e378294a2eeaffdbe
Moustafa <bendib.moustafa@gmail.com>
2019-11-27 11:46:52 +0100

    Adding Role authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -34,4 +34,14 @@ exports.protect = asyncHandler(async (req, res, next) => {
         return next(new ErrorResponse('Not authorised to access this route', 401));
     }
 
-})
+});
+
+// Grant access to specific roles
+exports.authorize = (...roles) => {
+    return (req,res,next) => {
+        if(!roles.includes(req.user.role)) {
+            return next(new ErrorResponse(`User role ${req.user.role} is not authorized to access rhis route`, 401));
+        }
+        next();
+    }
+}
