b435185043251cbd9337a63c27574f4837352f3c
Moustafa <bendib.moustafa@gmail.com>
2019-11-29 23:18:22 +0100

    Add update details and update password when user is logged in

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {register, login, getMe, forgotPassword, resetPassword} = require('../controllers/auth');
+const {register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword} = require('../controllers/auth');
 
 const router = express.Router();
 
@@ -10,6 +10,10 @@ router.post('/register', register);
 router.post('/login', login);
 
 router.get('/me',protect, getMe);
+
+router.put('/updatedetails',protect, updateDetails);
+
+router.put('/updatePassword',protect, updatePassword);
 
 router.post('/forgotpassword', forgotPassword);
 
